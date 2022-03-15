7e660094878de57a88dbaa504ea5a2f8f570e128
SoumyaMathe <94243283+SoumyaMathe@users.noreply.github.com>
2022-03-15 10:28:24 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,22 @@
+#1
+r=float(input("enter the radius of circle="))
+a=3.14*r*r
+print("area of circle=",a)
+  #2      
+name1=input("enter first name: ")
+name2=input("enter last name: ")
+print(name2,name1)
+#3
+for i in range(4,9):
+    f=print(i*i)
+
+#4a
+for i in range(1,21):
+    print("good programmer")
+
+    
+
+#c
+n=int(input("enter the no of rows"))
+for i in range(1,n+1):
+    print(i*"*")
